09b7d5ca58731bc29c2b70d240b44cad0f73287e
KirillOst13 <kirillost225@yandex.ru>
2024-09-10 10:13:22 +0500

    task2.c

--- labs/lab1/task2.c
+++ labs/lab1/task2.c
@@ -5,4 +5,12 @@ int main()
 	int a = 2, b = 5;
 	c = a + b;
 	printf("c=%d\n", a + b);
+
+	a = 3, b = 6;
+	c = a + b;
+	printf("c=%d\n", a + b);
+
+	a = 1, b = 3;
+	c = a + b;
+	printf("c=%d\n", a + b);
 }
