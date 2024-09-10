89d4d9887ff89c84ecfc8fd43d1487e2090c3f0d
KirillOst13 <kirillost225@yandex.ru>
2024-09-10 10:24:38 +0500

    Add files via upload

---
+++ labs/lab1/task3.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main()
+{
+  int a;
+  printf("Input a: ");
+  scanf("%d", &a);
+  printf("a = %d", a);
+  printf("\n");
+  int b;
+  printf("Input b: ");
+  scanf("%d", &b);
+  printf("b = %d", b);
+  printf("\n");
+  int c;
+  printf("Input c: ");
+  scanf("%d", &c);
+  printf("c = %d", c);
+}
